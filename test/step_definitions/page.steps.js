0672919f663b393e8a28757d0b0ae64f5053a4d1
Mischa Dasberg <mischa@dasberg.nl>
2020-05-19 18:17:28 +0200

    chore: cleanup

--- test/step_definitions/page.steps.js
+++ test/step_definitions/page.steps.js
@@ -6,103 +6,73 @@ import {expect} from "chai";
 
 const mocksDirectory = './node_modules/@ng-apimock/test-application/mocks';
 
-Given(/^I open the test page$/, openTestPage);
+Given(/^I open the test page$/, () => PagePO.open());
 
-When(/^I download the binary file$/, downloadTheBinaryFile);
-When(/^I enter (.*) and post the item$/, enterAndPostItem);
-When(/^I get the items$/, getTheItems);
-When(/^I get the items as jsonp$/, getTheItemsAsJsonp);
-
-Then(/^the items are fetched$/, checkItemsAreFetched);
-Then(/^the items are not yet fetched$/, checkItemsAreNotYetFetched);
-Then(/^the response is interpolated with variable (.*)$/, checkResponseIsInterpolatedWithVariable);
-Then(/^the (.*) response is downloaded$/, checkResponseIsDownloaded);
-Then(/^the (.*) response is returned for get items$/, checkReturnedResponseForGetItems);
-Then(/^the (.*) response is returned for post item$/, checkReturnedResponseForPostItem);
-
-function checkItemsAreNotYetFetched() {
-    return PagePO.done.contains('false');
-}
-
-function checkItemsAreFetched() {
-    return PagePO.done.contains('true');
-}
-
-function checkResponseIsInterpolatedWithVariable(variable) {
-    return PagePO.data.contains(variable);
-}
-
-function checkResponseIsDownloaded(scenario) {
+When(/^I download the binary file$/, () => {
     // @todo implement
-}
+});
 
-function checkReturnedResponseForGetItems(scenario) {
-    if (scenario === 'passThrough') {
-        return checkReturnedResponseForPassThrough();
-    } else {
-        return checkReturnedResponseForGetItemsScenario(scenario);
-    }
-}
+When(/^I enter (.*) and post the item$/, data => PagePO.input.type(data)
+    .then(() => PagePO.buttons.post.click()));
 
-function checkReturnedResponseForPostItem(scenario) {
-    if (scenario === 'passThrough') {
-        return checkReturnedResponseForPassThrough();
-    } else {
-        return checkReturnedResponseForPostItemScenario(scenario);
-    }
-}
+When(/^I get the items$/, () => PagePO.buttons.get.click());
 
-function checkReturnedResponseForGetItemsScenario(scenario) {
-    return cy
-        .readFile(path.join(mocksDirectory, 'get-items.mock.json'), {log: false})
-        .then((mocks) => PagePO.data
-            .should(($div) => {
-                if (mocks.responses[scenario].data !== undefined) {
-                    expect(JSON.parse($div.text())).to.deep.equal(mocks.responses[scenario].data)
-                }
-            })
-            .then(() => PagePO.status.should(($div) =>
-                expect(parseInt($div.text())).to.equal(mocks.responses[scenario].status)))
-        );
-}
+When(/^I get the items as jsonp$/, () => PagePO.buttons.getAsJsonp.click());
 
-function checkReturnedResponseForPassThrough() {
-    return PagePO.data
-        .should(($div) => expect(JSON.parse($div.text())).to.deep.equal(['passThrough']))
-        .then(() => PagePO.status.should(($div) => expect(parseInt($div.text())).to.equal(200)));
-}
+Then(/^the items are fetched$/, () => PagePO.done.contains('true'));
 
-function checkReturnedResponseForPostItemScenario(scenario) {
-    return cy
-        .readFile(path.join(mocksDirectory, 'post-item.mock.json'), {log: false})
-        .then((mocks) => PagePO.data
-            .should(($div) => {
-                if (mocks.responses[scenario].data !== undefined) {
-                    expect(JSON.parse($div.text())).to.deep.equal(mocks.responses[scenario].data)
-                }
-            })
-            .then(() => PagePO.status.should(($div) =>
-                expect(parseInt($div.text())).to.equal(mocks.responses[scenario].status)))
-        );
-}
+Then(/^the items are not yet fetched$/, () => PagePO.done.contains('false'));
 
-function downloadTheBinaryFile() {
+Then(/^the response is interpolated with variable (.*)$/,
+     variable => PagePO.data.contains(variable));
+
+Then(/^the (.*) response is downloaded$/, scenario => {
     // @todo implement
-}
+});
 
-function enterAndPostItem(data) {
-    return PagePO.input.type(data)
-        .then(() => PagePO.buttons.post.click());
-}
+Then(/^the (.*) response is returned for get items$/, scenario => {
+    if (scenario === 'passThrough') {
+        return PagePO.data
+            .should(($div) => expect(JSON.parse($div.text())).to.deep.equal(['passThrough']))
+            .then(
+                () => PagePO.status.should(($div) => expect(parseInt($div.text())).to.equal(200)));
+    } else {
+        return cy
+            .readFile(path.join(mocksDirectory, 'get-items.mock.json'), {log: false})
+            .then((mocks) => PagePO.data
+                .should(($div) => {
+                    if (mocks.responses[scenario].data !== undefined) {
+                        expect(JSON.parse($div.text())).to.deep
+                            .equal(mocks.responses[scenario].data)
+                    }
+                })
+                .then(() => PagePO.status.should(($div) =>
+                                                     expect(parseInt($div.text())).to
+                                                         .equal(mocks.responses[scenario].status)))
+            );
+    }
+});
 
-function getTheItems() {
-    return PagePO.buttons.get.click();
-}
+Then(/^the (.*) response is returned for post item$/, scenario => {
+    if (scenario === 'passThrough') {
+        return PagePO.data
+            .should(($div) => expect(JSON.parse($div.text())).to.deep.equal(['passThrough']))
+            .then(
+                () => PagePO.status.should(($div) => expect(parseInt($div.text())).to.equal(200)));
+    } else {
+        return cy
+            .readFile(path.join(mocksDirectory, 'post-item.mock.json'), {log: false})
+            .then((mocks) => PagePO.data
+                .should(($div) => {
+                    if (mocks.responses[scenario].data !== undefined) {
+                        expect(JSON.parse($div.text())).to.deep
+                            .equal(mocks.responses[scenario].data)
+                    }
+                })
+                .then(() => PagePO.status.should(($div) =>
+                                                     expect(parseInt($div.text())).to
+                                                         .equal(mocks.responses[scenario].status)))
+            );
+    }
+});
 
-function getTheItemsAsJsonp() {
-    return PagePO.buttons.getAsJsonp.click();
-}
-
-function openTestPage() {
-    return PagePO.open();
-}
